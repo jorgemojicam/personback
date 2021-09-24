970faa5e522281f8ea872a35e76bbde39fc287a8
Jorge Mojica <jorge.mojica92@gmail.com>
2021-09-24 11:39:34 -0500

    cambios

--- db/modulos.js
+++ db/modulos.js
@@ -21,9 +21,11 @@ async function get(context) {
 module.exports.get = get;
 
 async function getByRol(context) {
-    let sqlquery = `SELECT 
-                    mo.nombre_mod,
+    let sqlquery = `SELECT
                     mo.id_mod,
+                    mo.nombre_mod,
+                    mo.ruta_mod,
+                    mo.icono_mod,
                     (SELECT id_prol FROM permisosroles WHERE idmodulo_prol = mo.id_mod AND idrol_prol = ${context.idrol}) id,
                     (SELECT ver_prol FROM permisosroles WHERE idmodulo_prol = mo.id_mod AND idrol_prol = ${context.idrol}) ver,
                     (SELECT editar_prol  FROM permisosroles WHERE idmodulo_prol = mo.id_mod AND idrol_prol = ${context.idrol}) editar,
